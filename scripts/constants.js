d76715f3dfdee1bbe82d523394aa954c02f41f49
Erik Escoffier <me@erik.nz>
2023-03-27 11:47:05 +0200

    gd

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,3 +1,5 @@
+export const GD = './data/gd.json'
+export const SIMPLIFIED_REGIONS = './data/simplified_regions.geojson'
 export const SPECIES = './data/species.json'
 export const SPECIES_DATA = './data/species_data.json'
 export const SPECIES_MEDIA = './data/species_media.json'
